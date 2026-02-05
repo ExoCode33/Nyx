a5302178b23c6ba45cbc1397496dc8f425b156d9
ExoCode33 <emmanuel@supracode.ca>
2026-02-05 15:19:07 -0500

    Update pool.js

--- src/database/pool.js
+++ src/database/pool.js
@@ -4,8 +4,8 @@
  */
 
 const { Pool } = require('pg');
-const config = require('../config');
-const { logger, logError } = require('./logger');
+const config = require('../../config');
+const { logger, logError } = require('../utils/logger');
 
 class Database {
   constructor() {
